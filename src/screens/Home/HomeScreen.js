77033b969f53d51632d2976a000bb840e1db84e5
GuilhermeSilva236 <ggui2561@gmail.com>
2023-11-08 14:16:17 -0300

    terminei a estrutura da pagina home

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,38 +1,58 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import PropTypes from 'prop-types';
 
 const HomeScreen = () => {
-    const cards = [
+    // Array para as imagens do carrossel
+    const carouselImages = [
         {
             id: 1,
-            image: require('../../../assets/images/passageiro.png'),
+            image: require('../../../assets/images/img1.png'), // Substitua pelo caminho da imagem do carrossel
+        },
+        {
+            id: 2,
+            image: require('../../../assets/images/1.png'), // Substitua pelo caminho da imagem do carrossel
+        },
+        {
+            id: 3,
+            image: require('../../../assets/images/1.png'), // Substitua pelo caminho da imagem do carrossel
+        },
+        
+        // Adicione mais objetos conforme necessário
+    ];
+
+    // Array para os quadrados pretos
+    const passengerSquares = [
+        {
+            id: 1,
+            image: require('../../../assets/images/passageiro.png'), // Substitua pelo caminho da imagem do quadrado 1
             text: 'Quadrado 1',
         },
         {
             id: 2,
-            image: require('../../../assets/images/passageiro.png'),
+            image: require('../../../assets/images/passageiro.png'), // Substitua pelo caminho da imagem do quadrado 2
             text: 'Quadrado 2',
         },
         {
             id: 3,
-            image: require('../../../assets/images/passageiro.png'),
+            image: require('../../../assets/images/passageiro.png'), // Substitua pelo caminho da imagem do quadrado 3
             text: 'Quadrado 3',
         },
         {
             id: 4,
-            image: require('../../../assets/images/passageiro.png'),
+            image: require('../../../assets/images/passageiro.png'), // Substitua pelo caminho da imagem do quadrado 4
             text: 'Quadrado 4',
         },
         {
             id: 5,
-            image: require('../../../assets/images/passageiro.png'),
+            image: require('../../../assets/images/passageiro.png'), // Substitua pelo caminho da imagem do quadrado 5
             text: 'Quadrado 5',
-        }
+        },
+        // Adicione mais objetos conforme necessário
     ];
 
-    const renderItem = ({ item }) => {
+    const renderCarouselItem = ({ item }) => {
         return (
             <View style={styles.card}>
                 <Image source={item.image} style={styles.cardImage} />
@@ -40,28 +60,34 @@ const HomeScreen = () => {
         );
     };
 
+    const renderPassengerSquare = (item) => {
+        return (
+            <View key={item.id} style={styles.passengerSquare}>
+                <Image source={item.image} style={styles.squareImage} />
+                <View style={styles.textContainer}>
+                    <Text style={styles.whiteText}>{item.text}</Text>
+                </View>
+            </View>
+        );
+    };
+
     const { width } = Dimensions.get('window');
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.carouselContainer}>
                 <Carousel
-                    data={cards}
-                    renderItem={renderItem}
+                    data={carouselImages}
+                    renderItem={renderCarouselItem}
                     sliderWidth={width}
                     itemWidth={width}
                     layout="default"
                 />
             </View>
             <View style={styles.passengerSquareContainer}>
-                {cards.map((item, index) => (
-                    <View key={index} style={[styles.passengerSquare, index === 0 && styles.firstPassengerSquare]}>
-                        <Image source={item.image} style={styles.squareImage} />
-                        <Text style={styles.whiteText}>{item.text}</Text>
-                    </View>
-                ))}
+                {passengerSquares.map(renderPassengerSquare)}
             </View>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -72,22 +98,17 @@ HomeScreen.propTypes = {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 0,
-        marginTop: -400,
     },
     carouselContainer: {
         width: '100%',
-        height: 300, // Aumentei a altura para 300
+        height: 200,
         overflow: 'hidden',
         marginBottom: 20,
-        marginRight: 80,
-        marginLeft: 80,
+        marginHorizontal: -0,
     },
     card: {
         width: '100%',
-        height: 200, // Mantive a altura do card em 200
+        height: 200,
         marginBottom: 20,
         overflow: 'hidden',
     },
@@ -100,17 +121,14 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         flexWrap: 'wrap',
         padding: 10,
-        marginTop: 30,
+        marginTop: 20,
     },
     passengerSquare: {
         width: '48%',
         height: 100,
         borderRadius: 8,
         backgroundColor: 'black',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
+        justifyContent: 'flex-end', // Alinhar o conteúdo na parte inferior
         marginBottom: 20,
     },
     firstPassengerSquare: {
@@ -119,10 +137,17 @@ const styles = StyleSheet.create({
     squareImage: {
         width: 70,
         height: 70,
+        marginLeft: 120,
+  
     },
     whiteText: {
         color: 'white',
         marginLeft: 10,
+    },
+    textContainer: {
+        padding: 10,
+        width: '100%',
+   
     },
 });
 
