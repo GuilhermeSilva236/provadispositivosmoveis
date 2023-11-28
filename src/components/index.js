d187122cd0590401e0c946c092519e66d474a5e4
GuilhermeSilva236 <ggui2561@gmail.com>
2023-11-28 16:26:19 -0300

    codigo finalizado apresentação

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-import Separador from "./Separador";
-import CategoriaMenuItem from "./CategoriaMenuItem";
+
 import StarRating from "./StarRating";
 
-export {Separador, CategoriaMenuItem, StarRating};
+export { StarRating};
--- src/renderizacao/index.js
+++ src/renderizacao/index.js
@@ -1,5 +1,4 @@
 import Colors from "./Colors";
-import Categorianome from "./Categorianome";
-import Images from "./Images";
 
-export {Colors, Categorianome, Images};
+
+export {Colors };
